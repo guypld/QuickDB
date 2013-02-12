de1bd9ddd2d16cadb5d78e846e43a98880aade2d
Guy Peled <peled.guy@gmail.com>
2013-02-12 23:32:14 +0200

    examples

--- QuickDB/src/new_test..clj
+++ QuickDB/src/new_test..clj
@@ -18,3 +18,30 @@
 	"dsdsdsd"
 	( [] (greeting "default"))
 	( [username] (str "hello " username)))
+
+; function with varible arity (this can be useful in recursive)
+(defn date [person-1 person-2 & chaperones]
+	(println person-1 "and" person-2
+		"went out with" (count chaperones) "chaperones." ))
+
+;Anonymous functions
+(filter (fn [w] (> (count w) 2)) (re-split #"\W+" "A fine day"))
+--> ("fine" "day")
+;SAME: [% is the 1st param]
+(filter #(> (count %) 2) (re-split #"\W+" "A fine day it is"))
+
+; making funcitons (by anon):
+(defn make-greeter [greeting-prefix]
+	(fn [username] (str greeting-prefix ", " username)))
+(def aloha-greeting (make-greeter "Aloha"))
+(aloha-greeting "world")
+--> "Aloha, world"
+
+; distructring
+(let [[x y] [1 2 3]]
+	[x y])
+--> [1 2]
+
+;if
+(defn is-small? [number]
+	(if (< number 100) "yes" "no" ))
