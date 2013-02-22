c69b491d9eccccf1166138eed085cd703ca8c8aa
Moshlvi <moshlvi@gmail.com>
2013-02-22 21:29:48 +0200

    create table function and tests

--- QuickDB/src/core/core.clj
+++ QuickDB/src/core/core.clj
@@ -1,17 +1,31 @@
 (ns core.core 
-  ;(:use [core.printDB])
-  ;(:use (utils.constants))
-  ;(:use (utils.utils))
+  (:use (core.printDB))
+  (:use (utils.constants))
+  (:use (utils.utils))
   )
 
 
+
 ;db with data for tests
-(def db(ref {:books {:keys ["id"] :cols ["id" "name" "year"] :data [{"id" 5 "name" "book" "year" 1999}
-                                                                    {"id" 2 "name" "english" "year" 2002}
-                                                                    ]}
-             :person {:keys ["name"] :cols ["id" "name" "age"] :data [{"id" 80 "name" "Moshe" "age" 26}
-                                                                    {"id" 123 "name" "Dror" "year" 20}
-                                                                    ]}}))
+(def db(ref {}))
+
+(defn create-table 
+  [tableName fields keys]
+  (cond
+    (not (nil? (db tableName))) (println utils.constants/msgErrTableNameExists)
+    (check-record-validation keys fields valid-cols)
+    (doall [
+            (dosync (alter db assoc tableName {:keys keys :cols fields :data {}}))
+            (println utils.constants/msgCreateTableSuccess)
+            ]
+      )
+  :else (println utils.constants/msgErrKeyNotInFields)
+  ))
+
+(defn dropTable 
+  [name] 
+  (dosync
+    (alter db dissoc name)))
 
 ;high order function- get 2 collections and function. 
 ;call the function with the first element in the 
@@ -19,14 +33,12 @@
 (defn check-record-validation 
   "call the function with the first element in the 
 first collection and with the second collection"
-  [checkList baseList func msg] 
+  [checkList baseList func] 
   (cond 
     (nil? checkList) true 
     (func (first checkList) baseList) (check-record-validation (next checkList) baseList func) 
-    :else (doall [
-                  (println msg) 
-                  (false)
-                  ]) ))
+    :else false
+    ) )
 
 ;get key and record and return true if the key is found in the record
 (defn valid-key
@@ -53,9 +65,39 @@ first collection and with the second collection"
   "get table name and new record. check record column name and keys validation.
 if the record is correct, add it to the table "
   [table newRecord] 
-(if (and
-  (check-record-validation ((db table) :keys) newRecord  valid-key utils.constants/msgErrInvalidKey)
-(check-record-validation (keys newRecord) ((db table) :cols) valid-cols utils.constants/msgErrInvalidfield))
-(add-record table newRecord)
+(cond
+  (not(check-record-validation ((db table) :keys) newRecord  valid-key))
+  (println utils.constants/msgErrInvalidKey)
+(not(check-record-validation (keys newRecord) ((db table) :cols) valid-cols ))
+(println utils.constants/msgErrInvalidfield)
+:else (doall[
+             (add-record table newRecord)
+             (println utils.constants/msgInsrtRecSuccess)
+             ]
+        )
 ))
 
+
+(def bookfields '("id" "name" "year"))
+(def bookkeys '("id"))
+(def personfields '("id" "name" "age"))
+(def personKeys '("id" "age"))
+(def dummyKeys '("phone"))
+
+(def r1 {"id" 5 "name" "book" "year" 1999})
+(def r2 {"id" 2 "name" "english" "year" 2002})
+(def r3 {"id" 80 "name" "Moshe" "age" 26})
+(def r4 {"id" 123 "name" "Dror" "age" 20})
+
+(create-table "books" bookfields dummyKeys)
+(create-table "books" bookfields bookKeys)
+(create-table "books" personfields personKeys)
+(create-table "person" personfields personKeys)
+
+(insert "books" r1)
+(insert "books" r2)
+(insert "books" r1)
+(insert "person" r1)
+(insert "person" r3)
+(insert "person" r4)
+(core.printDB/print-db)
--- QuickDB/src/utils/constants.clj
+++ QuickDB/src/utils/constants.clj
@@ -1,8 +1,11 @@
 (ns utils.constants)
 
+(def msgCreateTableSuccess "Table creates successfully")
+(def msgInsrtRecSuccess "Insert record is success")
 
 ;error masseges
 (def msgErrInvalidKey "Add record fail- The new record doesn't contain all keys")
 (def msgErrInvalidfield "Add record fail- The new record contain invalid feilds")
+(def msgErrTableNameExists "Create table error- table name already exists in DB")
+(def msgErrKeyNotInFields "Create table error- key doesn't exist in feilds")
 
-(defn p[] (+ 1 2))
