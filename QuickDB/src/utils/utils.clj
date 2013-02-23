3140ce3f8872e0055a9de509e1af94db1c73c33d
guypld <peled.guy@gmail.com>
2013-02-23 18:49:24 +0200

    Update QuickDB/src/utils/utils.clj
    removed func

--- QuickDB/src/utils/utils.clj
+++ QuickDB/src/utils/utils.clj
@@ -15,34 +15,6 @@
           (map = (keys m1) (keys m2)))
  )
 
-;util func- used by select function.
-(defn is-match?
-  "Check if some Col(represented by map) is matching to some Target Col.
-   This pure finction gets Source Col, and Target Col, and map of functions,
-   each funtion to each Col."
-  [source-map target-map cond-list]
-  (if (nil? cond-list)
-    true  ; if condition list empty - return true
-    (if (and ; cheak if all maps share same keys
-          (is-map-matched? source-map target-map)
-	        (is-map-matched? source-map cond-list))
-      (let [res true] 
-;???      
-      (doseq ; run over all keys and evaluate it 
-       [keyval cond-list] 
-       ; (f l1 l2)
-        (let [k (key keyval)] ;k is the curr key from cond-list
-         
-           ( (cond-list k) (source-map k) (target-map k) )
-           
-       )))
-
-     ;else
-     (println "Not all maps is same")
-    )
-  )
-) 
-  
 (defn do-func-with-all 
   [data coll func] 
   ( when-not (nil? coll) 
