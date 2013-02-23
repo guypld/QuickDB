2a8358d01523ccefd0bffdbf76e5c660ed9d65db
guypld <peled.guy@gmail.com>
2013-02-23 12:07:46 +0200

    Update QuickDB/src/utils/utils.clj
    added is-map-matched? function

--- QuickDB/src/utils/utils.clj
+++ QuickDB/src/utils/utils.clj
@@ -6,3 +6,11 @@
   "true if seq contains elm"
   [seq elm]  
   (some #(= elm %) seq))
+
+;util func- check if two maps conatains the same keys
+(defn is-map-matched? 
+  "Check if 2 maps share the same keys."
+  [m1 m2]
+  (every? true? 
+          (map = (keys m1) (keys m2)))
+ )
