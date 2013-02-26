05111e038684a5054b407b1ef74f8db876825c50
yano1985 <ronyanovich@walla.com>
2013-02-26 23:28:02 +0200

    Update QuickDB/src/tests/testFile.clj
    new test methodology
    TODO:
      test all execute commaneds
      test printing

--- QuickDB/src/tests/testFile.clj
+++ QuickDB/src/tests/testFile.clj
@@ -1,13 +1,11 @@
+
 (ns tests.testFile 
   (:use [core.core] [clojure.test] [core.printDB] [core.query] )
   )
 
-
 ; READ !!
 ; http://nakkaya.com/2009/11/18/unit-testing-in-clojure/ 
 ;(deftest 
-
-
 
 ;db with data for test
 ;(def db(ref {:books {:keys ["id"] :cols ["id" "name" "year"] :data [{"id" 5 "name" "book" "year" 1999}
@@ -17,34 +15,58 @@
     ;                                                                {"id" 123 "name" "Dror" "year" 20}
      ;                                                               ]}}))
 
+(def db-test-1(ref {"books" {:keys ["id"] :cols ["id" "name" "year"] :data []}}))
+(def db-test-2(ref {"books" {:keys ["id"] :cols ["id" "name" "year"] :data [{"id" 5 "name" "book" "year" 1999}]}}))
+(def db-test-3(ref {"books" {:keys ["id"] :cols ["id" "name" "year"] :data [{"id" 5 "name" "book" "year" 1999}
+                                                                            {"id" 2 "name" "english" "year" 2002}]}}))
+(def db-test-4(ref {}))
 
-;tests
-(def bookfields '("id" "name" "year"))
-(def bookkeys '("id"))
-(def personfields1 '("id" "name" "age"))
-(def personKeys '("id" "age"))
-(def dummyKeys '("phone"))
+(defn test-create-large-db []
+  
+  )
 
-(def r1 {"id" 5 "name" "book" "year" 1999})
-(def r2 {"id" 2 "name" "english" "year" 2002})
-(def r5 {"id" 80 "name" "Moshe" "age" 26})
-(def r4 {"id" 123 "name" "Dror" "age" 20})
- 
-(execute ["create table" "books" bookfields dummyKeys]) ;fail- key doesn't wxist in fields
-(execute ["create table" "books" bookfields bookkeys]) ;success
-(execute ["create table" "books" personfields1 personKeys]) ;fail- table name exists
-(execute ["create table" "person" personfields1 personKeys]) ;success
+(deftest test-creation (is (= @db-test-1 @db)))
+(deftest test-insert-1 (is (= @db-test-2 @db)))
+(deftest test-insert-2 (is (= @db-test-3 @db)))
+(deftest test-after-drop (is (= @db-test-4 @db)))
+(deftest test-after-del (is (= @db-test-2 @db)))
 
-;(insert-all "books" [r1 r2 r1])
-(execute ["insert" "books" r2]) ;success
-(execute ["insert" "books" r1]) ;success
-(execute ["insert" "person" r1]) ;feild- doesn't have all keys
-(execute ["insert" "person" r5]) ;success
-(execute ["insert" "person" r4]) ;success
-(print-db db)
-;(execute ["drop" "books"])
-(print-db db)
-(print-table-return (execute ["select" "books" ["id"]]))
-; (execute ["insert" "books" {"id" 80 "name" "Moshe" "year" 26 }])
-; (execute ["select" "books" ["id"]])
-; (execute ["select where" "books" ["id"] {"id" >} {"id" 3}])
+(def table-name "books")
+
+(deftest test-insert
+  (execute ["create table" table-name '("id" "name" "year") '("id")])
+  (test-creation)
+  (execute ["insert" table-name {"id" 5 "name" "book" "year" 1999}])
+  (test-insert-1)
+  (execute ["insert" table-name {"id" 2 "name" "english" "year" 2002}])
+  (test-insert-2))
+
+(deftest test-insert-delete
+  (execute ["create table" table-name '("id" "name" "year") '("id")])
+  (test-creation)
+  (execute ["insert" table-name {"id" 5 "name" "book" "year" 1999}])
+  (test-insert-1)
+  (execute ["insert" table-name {"id" 2 "name" "english" "year" 2002}])
+  (test-insert-2)
+  (execute ["delete" table-name {"id" 2 "name" "english" "year" 2002}])
+  (test-after-del)
+  )
+
+;drop , delete , insert , select , create table , select where"
+
+;test list of all test cases
+(deftest test-list
+  ;test insertion of 2 recoreds
+  (test-insert)
+  (execute ["drop" table-name])
+  (test-after-drop)
+  
+  ;test insertion of 2 recoreds and delete one
+  (test-insert-delete)
+  (execute ["drop" table-name])
+  
+  
+  
+  )
+
+
